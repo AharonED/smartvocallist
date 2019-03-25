6644d873a31929c28938f0becacad008fa7ff77e
RonenIsGreat <40147121+RonenIsGreat@users.noreply.github.com>
2019-03-25 20:52:36 +0200

    temp items in the check lists activity

--- SmartVocalListProject/app/src/main/java/edu/cmu/pocketsphinx/demo/CheckListsActivity.java
+++ SmartVocalListProject/app/src/main/java/edu/cmu/pocketsphinx/demo/CheckListsActivity.java
@@ -3,15 +3,24 @@ package edu.cmu.pocketsphinx.demo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-import java.util.List;
+import java.util.ArrayList;
+
+import DataObjects.Checklist;
+import Model.Model;
+
 
 public class CheckListsActivity extends AppCompatActivity {
+    private ListView checkListsView;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> checkListsDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +30,42 @@ public class CheckListsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent myIntent = new Intent(CheckListsActivity.this, Add_list_activity.class);
-                startActivity(myIntent);
-            }
+        fab.setOnClickListener(view -> {
+            Intent myIntent = new Intent(CheckListsActivity.this, Add_list_activity.class);
+            startActivity(myIntent);
         });
 
-        ListView checkListsView = findViewById(R.id.checkListsView);
+        checkListsView = findViewById(R.id.checkListsView);
+        checkListsDisplay = getCheckListsToDisplay();
+        adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, checkListsDisplay);
+        checkListsView.setAdapter(adapter);
+        checkListsView.setClickable(true);
+        checkListsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), checkListsDisplay.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
+    private ArrayList<String> getCheckListsToDisplay(){
+        ArrayList<String> temp = new ArrayList<>();
+        temp.add("first item");
+        temp.add("2 item");
+        temp.add("3 item");
+        temp.add("ronen is the best");
+        temp.add("ronen is the best");
+        temp.add("ronen is the best");
+        temp.add("ronen is the best");
+        temp.add("ronen is the best");
+        temp.add("ronen is the best");
+        temp.add("ronen is the best");
+        temp.add("ronen is the best");
+        temp.add("ronen is the best");
+        temp.add("ronen is the best");
+        temp.add("ronen is the best");
+        temp.add("ronen is the best");
+        temp.add("ronen is the best");
+        temp.add("ronen is the best");
+        return temp;
+    }
 }
