39afae906af51fa2dbd5dc7ca8921f8ad0e02aab
Aharon <aharon@ald.co.il>
2019-04-08 22:07:51 +0300

    -

--- SmartVocalListProject/app/src/main/java/Model/ModelChecklists.java
+++ SmartVocalListProject/app/src/main/java/Model/ModelChecklists.java
@@ -35,24 +35,36 @@ public class ModelChecklists  extends Model<Checklist> implements Serializable {
     @TargetApi(Build.VERSION_CODES.N)
     public void getItemsAsync(ItemsLsnr<Checklist> lsnr) {
         Repository rep = new Repository();
-        items =  rep.GetChecklists(lsnr);
-        ArrayList<Checklist> chks = items;
-        ModelChecklistItems.getInstance().getItemsAsync(new ItemsLsnr<ChecklistItem>() {
-            @Override
-            public void getItemsLsnr(ArrayList<ChecklistItem> items) {
-                for (Checklist chk: chks) {
+        //items =  rep.GetChecklists(lsnr);
+
+        items =  rep.GetChecklists(new ItemsLsnr<Checklist>() {
+                                       @Override
+                                       public void getItemsLsnr(ArrayList<Checklist> items) {
+
+                                           ArrayList < Checklist > chks = items;
+                                           ModelChecklistItems.getInstance().getItemsAsync(new ItemsLsnr<ChecklistItem>() {
+                                               @Override
+                                               public void getItemsLsnr(ArrayList<ChecklistItem> items) {
+                                                   for (Checklist chk: chks) {
 //                    ArrayList<ChecklistItem> tmp = (ArrayList<ChecklistItem>) items.stream().filter(o -> o.getChecklistId().equals(chk.getId()));
 
-                    Iterator<ChecklistItem> iterator = items.iterator();
-                    while (iterator.hasNext()) {
-                        ChecklistItem item = iterator.next();
-                        if (item.getChecklistId() != null &&  item.getChecklistId().equals(chk.getId())) {
-                            chk.getChecklistItems().add(item);
-                        }
-                    }
-                }
-            }
-        });
+                                                       Iterator<ChecklistItem> iterator = items.iterator();
+                                                       while (iterator.hasNext()) {
+                                                           ChecklistItem item = iterator.next();
+                                                           if (item.getChecklistId() != null &&  item.getChecklistId().equals(chk.getId())) {
+                                                               chk.getChecklistItems().add(item);
+                                                           }
+                                                       }
+                                                   }
+                                               }
+                                           });
+
+                                           lsnr.getItemsLsnr(items);
+                                           //::items = items;
+                                       }
+                                   });
+
+
     }
 //
 }
