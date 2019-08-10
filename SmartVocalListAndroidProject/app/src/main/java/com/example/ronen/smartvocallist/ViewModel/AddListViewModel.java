4f21518c2627164a65d1e3df1a138eb78012277d
RonenIsGreat <40147121+RonenIsGreat@users.noreply.github.com>
2019-08-10 23:31:00 +0300

    fix add list view model

--- SmartVocalListAndroidProject/app/src/main/java/com/example/ronen/smartvocallist/ViewModel/AddListViewModel.java
+++ SmartVocalListAndroidProject/app/src/main/java/com/example/ronen/smartvocallist/ViewModel/AddListViewModel.java
@@ -13,55 +13,18 @@ import java.util.Comparator;
 import java.util.List;
 
 public class AddListViewModel extends ViewModel {
-    MutableLiveData<List<Checklist>> allChecklists;
-    ModelChecklists model;
-    public MutableLiveData<Checklist> CurrentChecklist;
+    public Checklist CurrentChecklist;
 
     public AddListViewModel(){
-        allChecklists = new MutableLiveData<>();
-        model =  ModelChecklists.getInstance();
-        model.getItemsAsync(this::checkListsToDisplay);
-        CurrentChecklist = new MutableLiveData<Checklist>();
     }
+
     public Checklist getCurrentChecklist()
     {
-        return CurrentChecklist.getValue();
+        return CurrentChecklist;
     }
 
     public void setCurrentChecklist(Checklist newChecklist)
     {
-        if (this.CurrentChecklist == null)
-        {
-
-        }
-        this.CurrentChecklist.setValue(newChecklist);
-
-    }
-
-    public ModelChecklists getModel(){
-        return model;
-    }
-
-    public MutableLiveData<List<Checklist>> getData() {
-        return allChecklists;
-    }
-
-    public void displayLocalReportedChecklists() {
-        model.getLocalChecklistAsync(ModelChecklists -> {
-            checkListsToDisplay(ModelChecklists);
-        });
-    }
-
-    private void checkListsToDisplay(ArrayList<Checklist> checklists) {
-        Collections.sort(checklists, new Comparator<Checklist>() {
-            @Override
-            public int compare(Checklist checkList1, Checklist checkList2) {
-                Double chk1LastUpdate = checkList1.getLastUpdate();
-                Double chk2LastUpdate = checkList2.getLastUpdate();
-                return chk2LastUpdate.compareTo(chk1LastUpdate);
-            }
-        });
-
-        allChecklists.setValue(checklists);
+        this.CurrentChecklist = newChecklist;
     }
 }
